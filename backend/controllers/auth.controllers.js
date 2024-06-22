907b45bacc144837345fb35f2d3cfe835a161e11
Anuradha Naidu <anuradhaknaidu6@gmail.com>
2024-06-23 01:43:38 +0530

    initial commit

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,30 +2,31 @@ import User from '../models/user.models.js';
 import bcrypt from "bcryptjs";
 
 export const signup = async(req, res) => {
- try {
-  const {fullName, userName, password, confirmPassword, gender} = req.body;
+ try { 
+  const {fullName, userName, password, confirmPassword, gender} = req.body; //inputs from the user
 
   if(password !== confirmPassword){
-   return res.status(400).json({error:"Password and Confirm Password do not match!"})
+   return res.status(400).json({error:"Password and Confirm Password do not match!"}) //check this condition 
   }
 
-  const user = await User.findOne({userName});
-
+  const user = await User.findOne({userName}); 
+  //check if user exists in database
   if(user){
-   return res.status(400).json({error:"Username alreadys exists"})
+   return res.status(400).json({error:"Username alreadys exists"}) 
   }
 
-  //hash password here 
+  //hash password here  
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
   //https://avatar-placeholder.iran.liara.run/
   //https://avatar.iran.liara.run/public/68
 
+  //determined the user profile 
   const girlProficPic = `https://avatar.iran.liara.run/public/girl?username=${userName}`
   const boyProficPic = `https://avatar.iran.liara.run/public/boy?username=${userName}`
 
-  const newUser = new User({
+  const newUser = new User({  //created User with all above inputs passed
    fullName,
    userName,
    password: hashedPassword,
